b10a05e97e325a7afa5e6446fbacc2c17c891e65
kinetic4 <pratyushbhargava6@gmail.com>
2026-04-29 20:23:24 +0530

    Add Navbar component to layout and comment out title in page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <h1 className='text-2xl underline'>Bookilied</h1>
+      {/* <h1 className='text-2xl underline'>Bookilied</h1> */}
     </div>
   )
 }
