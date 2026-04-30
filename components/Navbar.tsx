c925f480ad677abc0e8854a21e7f9160ca74d099
kinetic4 <pratyushbhargava6@gmail.com>
2026-04-30 21:48:29 +0530

    Refactor Navbar component: Implement dynamic navigation links and active state highlighting

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,19 @@
+'use client'
+
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
+const navItem = [
+  {label: 'Liabrary', href: '/'},
+  {label: 'Add new', href: '/books/new'}
+]
+
 const Navbar = () => {
+  const pathName = usePathname()
+
   return (
     <div className="w-full fixed z-50 bg-('--bg-primary') text-('--text-primary')">
       <div className="wrapper navbar-height py-4 flex justify-between items-center">
@@ -13,7 +24,15 @@ const Navbar = () => {
            </span>
         </Link>
         <nav className='w-fit flex gap-7 items-center'>
+          {navItem.map(({label, href}) => {
+            const isActive = pathName === href || (href != '/' && pathName.startsWith(href))
 
+            return (
+              <Link href={href} key={label} className={cn('nav-link-base', isActive ? 'nav-link-active' : 'text-black hover:opacity-70')}>
+                {label}
+              </Link>
+            )
+          })}
         </nav>
       </div>
     </div>
