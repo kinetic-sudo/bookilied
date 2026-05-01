543a01b82fbfb2eccb50c5273e0994436345a436
kinetic4 <pratyushbhargava6@gmail.com>
2026-05-01 15:50:25 +0530

    Add BookCard component to page: Display a grid of sample books in the library section.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import BookCard from '@/components/BookCard'
 import HomePageSection from '@/components/HomePageSection'
+import { sampleBooks } from '@/lib/constant'
 
 const Page = () => {
   return (
     <main>
       <HomePageSection />
+      <div className="library-hero-grid">
+        {sampleBooks.map((book) => (
+          <BookCard />
+        ))}
+      </div>
     </main>
 
   )
