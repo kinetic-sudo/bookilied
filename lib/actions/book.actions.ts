90e548d3de7a1ee1deb762c06543b27e073ff849
kinetic4 <pratyushbhargava6@gmail.com>
2026-05-04 18:51:38 +0530

    Implement book creation logic: Add slug uniqueness check and serialize existing book data to enhance the createBook function in book.actions.ts.

--- lib/actions/book.actions.ts
+++ lib/actions/book.actions.ts
@@ -1,7 +1,8 @@
 import { CreateBook } from "@/types";
 import { success } from "zod";
 import { connectToDatabase } from "@/database/mongoose";
-import { generateSlug } from "../utils";
+import { generateSlug, serializeData } from "../utils";
+import Book from "@/database/models/books.models";
 
 
 
@@ -9,6 +10,25 @@ export const createBook = async (data: CreateBook) => {
     try {
         await connectToDatabase();
         const slug = generateSlug(data.title)
+        const existingBook = await Book.findOne({slug}).lean();
+
+        if(existingBook) {
+            return {
+                success: false,
+                data: serializeData(existingBook),
+                alreadyExists: true
+            }
+        }
+
+        // TODO: check if subscription limit before creating a book
+
+        const book = await Book.create({
+            ...data, slug, totalSegments: 0,
+        });
+        return({
+            success: true,
+            data: serializeData(book),
+        })
     } catch (e) {
         console.error('error creating book', e);
         return {
