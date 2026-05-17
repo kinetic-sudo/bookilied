da4335ce0a42d324265ea07f2fa904644c86875f
kinetic4 <pratyushbhargava6@gmail.com>
2026-05-17 12:23:00 +0530

    feat: implement voice session creation in startVoicesession function, adding database integration for session management

--- lib/actions/session.action.ts
+++ lib/actions/session.action.ts
@@ -1,12 +1,18 @@
 'use server'
 
+import VoiceSession from "@/database/models/voiceSessions.models";
 import { connectToDatabase } from "@/database/mongoose"
+
 
 export const startVoicesession = async (clerkId, bookId) : Promise<StartSessionResult> => {
     try {
         await connectToDatabase();
 
         //limit/plan  to see whether a session is allowed 
+        const ssession = await VoiceSession.create({ clerkId, bookId, 
+            startedAt: new Date(), 
+            billingPeriodStart: getCurrentBillingPeriodStart()
+        });
     } catch (e) {
         console.error('error starting voice session', e)
         return {success: false, error: 'Failed to start session. Please try again later'}
