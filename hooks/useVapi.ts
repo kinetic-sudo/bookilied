fd058dad638ba7057df4c79de9642ecbe41cdc79
kinetic4 <pratyushbhargava6@gmail.com>
2026-05-19 22:30:28 +0530

    fix: correct logical condition for isActive in useVapi hook to ensure accurate status evaluation

--- hooks/useVapi.ts
+++ hooks/useVapi.ts
@@ -52,7 +52,7 @@ export const useVapi = (book: IBook) => {
     const voice = book.persona || DEFAULT_VOICE
     // const maxDuration = useLatestRef(limits.maxSessionMinutes * 60)
 
-    const isActive = status === 'listening' || status === 'thinking' || status === 'speaking' || 'starting'
+    const isActive = status === 'listening' || status === 'thinking' || status === 'speaking' || status === 'starting'
     
     //limits
     // const maxDurationRef = useLatestRef(limits.maxSessionMinutes * 60) 
