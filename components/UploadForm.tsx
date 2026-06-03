7d8fedb69ded08bdbf89f3a79dbe03799c2ef5cb
kinetic4 <pratyushbhargava6@gmail.com>
2026-06-03 22:17:28 +0530

    fix: enhance error handling in UploadForm to redirect to subscriptions on billing errors

--- components/UploadForm.tsx
+++ components/UploadForm.tsx
@@ -171,7 +171,9 @@ const UploadForm = () => {
       if(!book.success) {
         const msg =  typeof book.error === 'string' ? book.error : 'Failed to create book'
         toast.error(msg)
-        if((book as any))
+        if((book as any).isBillingError) {
+          router.push('/subscriptions')
+        }
         return
       }
       
